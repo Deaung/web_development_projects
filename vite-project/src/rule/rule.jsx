de91b9ae5cb88d2dc8bdb2c0878b57e34a06bc29
user <a76650051@gmail.com>
2025-04-03 20:23:12 +0900

    feat: 20250403 frontend project started

--- vite-project/src/rule/rule.jsx
+++ vite-project/src/rule/rule.jsx
@@ -4,10 +4,13 @@ export default function rule(){
   const [ time, setTime ] = useState('');
   const [ field, setField ] = useState('');
 
+  const enterField = (event) => {
+    setField(event.target.value)
+  }
   const enterTime = (event) => {
     setTime(event.target.value)
   }
-  const enterField = 
+  
 
   return(
     <>
